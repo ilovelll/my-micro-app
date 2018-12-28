052211c3803a844222864402298ce4118556be5c
Johny.li <johny.li@selmit.com>
2018-12-28 17:31:30 +0800

    update test case

--- test/uploadServices.test.js
+++ test/uploadServices.test.js
@@ -9,10 +9,32 @@ const Wreck = require('wreck');
 const Subtext = require('subtext');
 const micro = require('micro')
 const listen = require('test-listen')
-
+const { upload } = require('./micro-upload')
+const Boom = require('boom')
 const { UploadService } = require('../services/')
 
 const getUrl = fn => listen(micro(fn));
+
+
+const path = Path.join(__dirname, './file/image.jpg');
+const fileStream = fs.createReadStream(path);
+const fileContents = fs.readFileSync(path);
+
+const form = new FormData();
+form.append('file', fileStream);
+form.headers = form.getHeaders();
+
+const body = { body: form }
+
+const subtextUpload = async (req, res) => {
+  const { payload } = await Subtext.parse(req, null, { parse: true, output: 'stream' });
+  const file = payload.file
+  
+  if (!payload||!file) {
+    throw Boom.badRequest("no file uploaded")
+  }
+  micro.send(res, 200, { filename: file.hapi.filename})
+}
 
 describe('upload services:', () => {
   it('no file uploaded', async () => {
@@ -24,5 +46,15 @@ describe('upload services:', () => {
       statusCode: 400,
       error: 'Bad Request',
       message: 'no file uploaded'})
+  });
+
+  it('file uploaded', async () => {
+    expect.assertions(2);
+    const url = await getUrl(subtextUpload)
+    const res = await request.post(url, body).response
+    await expect(res.status).toBe(200);
+    await expect(res.json()).resolves.toEqual({ 
+      filename: 'image.jpg'
+    })
   });
 })
